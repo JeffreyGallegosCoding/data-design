3c0280437040746c09a9e02d2c6507bdaec101d9
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-09 16:59:43 -0700

    Began the Data Design project with it's base elements.

---
+++ epic/index.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>
+				Data Design Project
+			</title>
+		</head>
+		<h1>
+			Data Design Project
+		</h1>
+	</html>
